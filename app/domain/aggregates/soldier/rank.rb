141cf3bff2c3fda5bdc104c221a71d7d22198094
Julien Itard <julienitard@gmail.com>
2022-04-25 19:13:24 +0200

    Add presentation folders

---
+++ app/domain/aggregates/soldier/rank.rb
@@ -0,0 +1,12 @@
+# Aggregate Rank / Value Object
+
+require "app/domain/repositories/soldier"
+
+module Domain
+  module Aggregate
+    module Soldier
+      class Rank
+      end
+    end
+  end
+end
--- app/domain/aggregates/soldier/soldier.rb
+++ app/domain/aggregates/soldier/soldier.rb
@@ -1,11 +1,26 @@
 # AggregateRoot Soldier / Entity
 
 require "app/domain/repositories/soldier"
+require "app/domain/aggregates/soldier/rank"
 
 module Domain
   module Aggregate
     module Soldier
       class Soldier
+        def create_soldier(args*)
+          #TODO : Add validation
+
+          repository.add(args*)
+        end
+
+        def add_rank(soldier_id, rank)
+        end
+
+        private
+
+        def repository
+          @repository ||= Domain::Repository::Soldier
+        end
       end
     end
   end
